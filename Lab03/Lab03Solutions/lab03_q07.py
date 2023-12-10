7fc9c75feb57bdbc0c6e7bd05c140005c1ac2445
Chon Hou (Benny) Lao <bennylao830@gmail.com>
2023-12-10 21:53:49 +0000

    updated syntax

--- Lab03/Lab03Solutions/lab03_q07.py
+++ Lab03/Lab03Solutions/lab03_q07.py
@@ -1,7 +1,7 @@
 import sys
+
 arg_list = sys.argv
 no_args = len(sys.argv)
-
 
 
 def file_len(fname):
@@ -10,13 +10,13 @@ def file_len(fname):
             pass
     return i + 1
 
-not_readable_files  = []
-for i in range(1,no_args):
+
+not_readable_files = []
+for i in range(1, no_args):
     try:
         line_count = file_len(arg_list[i])
         print(arg_list[i], line_count)
     except:
         not_readable_files.append(arg_list[i])
-
 
 print('The following file(s) are not readable: ', not_readable_files)
