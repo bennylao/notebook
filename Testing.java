0b3181813c1e1618c15baa03a37d9c775154407c
Chon Hou (Benny) Lao <bennylao830@gmail.com>
2024-01-02 21:26:00 +0000

    updated java stuffs

--- Testing.java
+++ Testing.java
@@ -4,8 +4,6 @@ import java.util.Scanner;
 
 public class Testing {
     public static void main(String[] args) {
-      List<Integer> numbers = new ArrayList<>();
-      numbers.add(1);
-      int number = numbers.get(numbers.size());
+        System.out.println("Hello, World!");
     }
 }
