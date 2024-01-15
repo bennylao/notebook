4a95f25040fe850eac1459c4da5e6a7047428d4b
bennylao <bennylao830@gmail.com>
2024-01-15 00:14:18 +0000

    added pytorch tutorial

--- pythonStuffs/DeepLearning/pytorch_tutorial1.py
+++ pythonStuffs/DeepLearning/pytorch_tutorial1.py
@@ -113,7 +113,3 @@ print("Done!")
 # saving model
 torch.save(model.state_dict(), "model.pth")
 print("Saved PyTorch Model State to model.pth")
-
-# loading model
-# model = NeuralNetwork().to(device)
-# model.load_state_dict(torch.load("model.pth"))
